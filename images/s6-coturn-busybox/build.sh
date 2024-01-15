0823ae4aeae7239c0bdbdb6e73bda5f15532ea73
Berne Campbell <berne.campbell@gmail.com>
2024-01-16 03:53:38 +1100

    s6-coturn-busybox update_use and chmod fixes

--- images/s6-coturn-busybox/build.sh
+++ images/s6-coturn-busybox/build.sh
@@ -9,6 +9,7 @@ _packages="sys-apps/busybox"
 configure_rootfs_build()
 {
     update_use 'sys-apps/busybox' '+make-symlinks +static'
+    update_use '+static-libs' '+minimal' '+static'
     # this runs in the builder, but as one of the last build steps the builder's /etc/passwd is copied to the custom root
     useradd busybox
 }
@@ -32,7 +33,7 @@ finish_rootfs_build()
     # copy busybox /etc which will have busybox service definition in /etc/service to "${_EMERGE_ROOT}"
     cp -a /config/etc "${_EMERGE_ROOT}"/
     # ensure busybox service's run script is executable
-    chmod +x $(find "${_EMERGE_ROOT}"/etc/service -name run)
+    chmod +x "$(find "${_EMERGE_ROOT}"/etc/service -name run)"
     # set up busybox service's finish script
     ln -s /etc/s6_finish_default "${_EMERGE_ROOT}"/etc/service/busybox/finish
     # enable service by adding it to the scan directory
