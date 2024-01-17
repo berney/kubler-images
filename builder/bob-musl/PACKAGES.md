8ede8497586ee72aec3dfe9d76656d7e32aa5e30
Berne Campbell <berne.campbell@gmail.com>
2024-01-17 13:57:06 +1100

    Build bob-musl builder with Docker outside of kubler

---
+++ builder/bob-musl/PACKAGES.md
@@ -0,0 +1,14 @@
+### berney/bob-musl:20240115
+
+Built: Wed Jan 17 11:01:48 AEDT 2024
+Image Size: 2.79GB
+
+#### Installed
+None.
+#### Inherited
+Package | USE Flags
+--------|----------
+**FROM scratch** |
+#### Purged
+- [x] Headers
+- [x] Static Libs
---
+++ builder/bob-musl/build.sh
@@ -0,0 +1,34 @@
+#
+# Build config, sourced by build-root.sh in the build container
+#
+
+#
+# This hook can be used to configure the build container itself, install packages, run any command, etc
+#
+configure_builder() {
+    ### example for a stage3 builder setup that should work out of the box on x86_64 and glibc stage3
+    fix_portage_profile_symlink
+    # install basics used by helper functions
+    eselect news read new 1> /dev/null
+    emerge app-portage/flaggie app-portage/eix app-portage/gentoolkit
+    configure_eix
+    mkdir -p /etc/portage/package.{accept_keywords,unmask,mask,use}
+    touch /etc/portage/package.accept_keywords/flaggie
+    echo 'LANG="en_US.utf8"' > /etc/env.d/02locale
+    env-update
+    source /etc/profile
+    # install default packages
+    # when using overlay1 docker storage the created hard link will trigger an error during openssh uninstall
+    [[ -f /usr/"${_LIB}"/misc/ssh-keysign ]] && rm /usr/"${_LIB}"/misc/ssh-keysign
+    update_use 'dev-vcs/git' '-perl'
+    update_use 'app-crypt/pinentry' '+ncurses'
+    update_use 'dev-libs/libpcre2' '+jit'
+    update_keywords 'dev-python/ssl-fetch' '+~amd64'
+    update_keywords 'app-admin/su-exec' '+~amd64'
+    emerge dev-vcs/git app-eselect/eselect-repository app-misc/jq app-shells/bash-completion
+    #install_git_postsync_hooks
+    [[ "${BOB_UPDATE_WORLD}" == true ]] && emerge -vuND world
+    add_overlay musl
+    add_overlay kubler https://github.com/edannenberg/kubler-overlay.git
+    emerge dev-lang/go
+}
