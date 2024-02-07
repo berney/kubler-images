2e23569423b20878e4f0756ceeaaa27cf811a06e
Berne Campbell <berne.campbell@gmail.com>
2024-02-08 05:46:03 +1100

    [nmap-musl-static] Update for current nmap-9999

--- images/nmap-musl-static/build.sh
+++ images/nmap-musl-static/build.sh
@@ -1,4 +1,4 @@
-#
+#' 
 # Kubler phase 1 config, pick installed packages and/or customize the build
 #
 
@@ -26,15 +26,15 @@ configure_bob()
     #add_layman_overlay libressl
     # libressl
     # https://wiki.gentoo.org/wiki/Project:LibreSSL
-    echo "-libressl"               >> /etc/portage/profile/use.stable.mask 
-    echo "-curl_ssl_libressl"      >> /etc/portage/profile/use.stable.mask 
+    #echo "-libressl"               >> /etc/portage/profile/use.stable.mask
+    #echo "-curl_ssl_libressl"      >> /etc/portage/profile/use.stable.mask
     #echo "=dev-libs/libressl-2.4*" >> /etc/portage/package.accept_keywords/libressl
-    emerge -f dev-libs/libressl
-    emerge -C dev-libs/openssl net-misc/wget
-    echo "dev-libs/openssl"        >> /etc/portage/package.mask/openssl
-    update_use '+libressl'
-    emerge -1 dev-libs/libressl net-misc/wget
-    
+    #emerge -F dev-libs/libressl net-misc/wget
+    #emerge -C dev-libs/openssl net-misc/wget
+    #echo "dev-libs/openssl"        >> /etc/portage/package.mask/openssl
+    #update_use '+libressl'
+    #emerge -1 dev-libs/libressl net-misc/wget
+
     # Add our custom overlay
     add_overlay berne https://github.com/berney/gentoo-overlay.git
     #add_overlay berne file:///config/gentoo-overlay
@@ -47,7 +47,8 @@ configure_bob()
     #update_keywords '=net-analyzer/nmap-7.60' '+~amd64'
     #update_keywords '=net-analyzer/nmap-9999' '+~amd64'
     update_keywords '=net-analyzer/nmap-9999' '+**'
-    update_use 'net-analyzer/nmap' '+ipv6' '+libressl' '+ncat' '-ndiff' '-nls' '-nmap-update' '+nping' '+nse' '+ssl' '-system-lua' '-zenmap' '+static' '+libssh2'
+    #update_use 'net-analyzer/nmap' '+ipv6' '+libressl' '+libssh2' '+ncat' '+nping' '+nse' '+ssl' '-system-lua' '+static'
+    update_use 'net-analyzer/nmap' '+ipv6' '+libssh2' '+ncat' '+ndiff' '+nping' '+nls' '+nse' '+ssl' '+static' '+symlink' '-zenmap'
     # global
     #   - seems to cause problems (seems to work again, except for debianutils)
     update_use '+static-libs' '+minimal' '+static'
@@ -67,7 +68,7 @@ configure_bob()
 
     # Need to unprovide libressl so that it will be rebuilt
     # This can be useful to install a package from a parent image again, it may be needed at build time
-    unprovide_package dev-libs/libressl
+    #unprovide_package dev-libs/libressl
     unprovide_package dev-libs/openssl
     unprovide_package sys-libs/zlib
 
@@ -92,9 +93,10 @@ configure_rootfs_build()
     provide_package 'sys-libs/zlib'
     provide_package 'sys-libs/ncurses'
     provide_package 'sys-libs/readline'
-    provide_package 'dev-libs/libpcre'
+    provide_package 'dev-lang/lua'
+    provide_package 'dev-libs/libpcre2'
     provide_package 'dev-libs/liblinear'
-    provide_package 'dev-libs/libressl'
+    provide_package 'dev-libs/openssl'
     provide_package 'net-libs/libssh2'
 
     # This can be useful to install a package from a parent image again, it may be needed at build time
@@ -102,7 +104,7 @@ configure_rootfs_build()
 
     # Only needed when ${_packages} is empty, initializes PACKAGES.md
     init_docs "berne/nmap-musl-static"
-    
+
     # emerge in EMERGE_ROOT, we should already have all the dependencies built
     :
 }
@@ -135,7 +137,7 @@ finish_rootfs_build()
 
     # Everything at ${_EMERGE_ROOT} will end up in the final image
     #cp -rp "${DISTRIBUTION_DIR}/bin/*" "${_EMERGE_ROOT}/usr/local/bin"
-    
+
     # Rice Rice Baby - rm stuff we don't want in the final image
     rm -rf "${_EMERGE_ROOT}"/etc
     rm -rf "${_EMERGE_ROOT}"/tmp
@@ -144,6 +146,8 @@ finish_rootfs_build()
     #mkdir -p "${_EMERGE_ROOT}"/etc
     # handle bug in portage when using custom root, user/groups created during install are not created at the custom root but on the host
     #cp -f /etc/{passwd,group} "${_EMERGE_ROOT}"/etc/
+
+    #mkdir "${_EMERGE_ROOT}"/data
 
     # After installing packages manually you might want to add an entry to PACKAGES.md
     #log_as_installed "manual install" "nmap-musl-static-${_nmap-musl-static_version}" "https://nmap-musl-static.org/"
