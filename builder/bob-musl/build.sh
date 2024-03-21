91f638aad3f42ee53e568f4cd0df834f8c23da4f
Berne Campbell <berne.campbell@gmail.com>
2024-03-21 16:06:30 +1100

    export EMERGE_DEFAULT_OPTS

--- builder/bob-musl/build.sh
+++ builder/bob-musl/build.sh
@@ -11,7 +11,9 @@ configure_builder() {
     # install basics used by helper functions
     eselect news read new 1> /dev/null
     emerge --info | grep EMERGE_DEFAULT_OPTS
-    emerge --quiet-build app-portage/flaggie app-portage/eix app-portage/gentoolkit
+    export EMERGE_DEFAULT_OPTS="-b -k --quiet-build --tree"
+    emerge --info | grep EMERGE_DEFAULT_OPTS
+    emerge app-portage/flaggie app-portage/eix app-portage/gentoolkit
     configure_eix
     mkdir -p /etc/portage/package.{accept_keywords,unmask,mask,use}
     touch /etc/portage/package.accept_keywords/flaggie
@@ -26,18 +28,15 @@ configure_builder() {
     update_use 'dev-libs/libpcre2' '+jit'
     update_keywords 'dev-python/ssl-fetch' '+~amd64'
     update_keywords 'app-admin/su-exec' '+~amd64'
-    emerge --info | grep EMERGE_DEFAULT_OPTS
-    emerge --quiet-build dev-vcs/git app-eselect/eselect-repository app-misc/jq app-shells/bash-completion
+    emerge dev-vcs/git app-eselect/eselect-repository app-misc/jq app-shells/bash-completion
     #install_git_postsync_hooks
     [[ "${BOB_UPDATE_WORLD}" == true ]] && emerge -uND world
     add_overlay musl
     add_overlay kubler https://github.com/edannenberg/kubler-overlay.git
-    emerge --info | grep EMERGE_DEFAULT_OPTS
-    emerge --quiet-build dev-lang/go
+    emerge dev-lang/go
     # bdawg favs - I want these when doing `kubler build -i something`
     # Workaround vim nls issues on musl
     update_use app-editors/vim-core -nls
     update_use app-editors/vim -nls
-    emerge --info | grep EMERGE_DEFAULT_OPTS
-    emerge -quiet-build app-editors/vim sys-apps/bat sys-apps/fd sys-apps/ripgrep
+    emerge app-editors/vim sys-apps/bat sys-apps/fd sys-apps/ripgrep
 }
