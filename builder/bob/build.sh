a81a04e76da8a8d05783708c4454c6f227e9b0bc
Berne Campbell <berne.campbell@gmail.com>
2024-03-25 12:45:39 +1100

    Set EMERGE_DEFAULT_OPTS again

--- builder/bob/build.sh
+++ builder/bob/build.sh
@@ -30,6 +30,9 @@ configure_builder() {
     update_use 'app-crypt/pinentry' '+ncurses'
     update_keywords 'dev-python/ssl-fetch' '+~amd64'
     update_keywords 'app-admin/su-exec' '+~amd64'
+    emerge --info | grep EMERGE_DEFAULT_OPTS
+    export EMERGE_DEFAULT_OPTS="-b -k --quiet-build --tree"
+    emerge --info | grep EMERGE_DEFAULT_OPTS
     emerge dev-vcs/git app-eselect/eselect-repository app-misc/jq app-shells/bash-completion
     #install_git_postsync_hooks
     [[ "${BOB_UPDATE_WORLD}" == true ]] && emerge -vuND world
